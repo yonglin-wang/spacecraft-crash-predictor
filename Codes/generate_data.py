3775b72e96d7b6a1b06ba0c5ea2016fe04ee1203
Jie Tang <jietang@brandeis.edu>
2021-01-11 15:35:31 -0500

    Add files via upload

--- Codes/generate_data.py
+++ Codes/generate_data.py
@@ -12,30 +12,57 @@ warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 
 ####### define my own sliding window function
 def sliding_window(data, buck_size, step):
+    '''
+    author: Jie Tang
+    inputs:
+        data: the data we will do sliding windows
+        buck_size: the length of time series points used for our training, e.g. 500ms, 700ms etc.
+        step: the step we will move when we do sliding window
+    '''
     list_all = []
     tmp = []
-    if data.iloc[-1] - data.iloc[0] < buck_size:
-        return  tmp
+
+    ## determine the length of our data, if it's <= 1, we could not do sliding windows
+    if len(data)<=1:
+        return tmp
+
+    ## determine if the total time points length is less than our buck_size
+    if data.iloc[-1] - data.iloc[0] <= buck_size:
+        return tmp 
     else:
         temp_len = len(data[data.between(data.iloc[0], data.iloc[0] + buck_size)])
         for i in range(len(data)): 
             left = data.iloc[i] + i * step 
             right = data.iloc[i] + i * step  + buck_size
             series1 = data[data.between(left, right)] 
+            ## if the series length is less than 2, we need to pass it
             if len(series1)<2:
                 continue
-            if data.iloc[-1] - left < 0.5:
+            ## need to determine the boundary that last time series and left boundary is enough to do sliding ( greater than buck_size)
+            ## or we need to break the loop, since the data points are not enough to do next sliding. 
+            if data.iloc[-1] - left < buck_size:
                 break 
             list_all.append(series1)
         return list_all
 
 
 
-def generate_features(time_scale_train = 0.5,
-    time_ahead = 0.5,
-    sampling_rate = 50,
-    time_gap=3,
-    time_step = 0.3):
+def generate_features(time_scale_train,
+    time_ahead,
+    sampling_rate,
+    time_gap,
+    time_step):
+    '''
+    author: Jie Tang
+    inputs:
+        time_scale_train: the length of time used for training. e.g 500ms, 700ms
+        time_ahead: time in advance to predict
+        sampling_rate: sampling rate in each time_scale_train, our default value is 50
+        time_gap: this is used to exclude those consecutive crash events happened less than time_gap. Since when two crash events happend so closely,
+        we could not generate enough data to train
+        time_step: the step we will move when we do sliding window
+    '''
+
     np.set_printoptions(suppress=True)
 
     for_names = str(int(time_ahead*1000))
@@ -117,17 +144,14 @@ def generate_features(time_scale_train = 0.5,
                                 [crash_feature_label_300ms_500ms_test, pd.DataFrame([[arr1, arr2, arr3, arr4, arr5, arr6]],\
                                 columns=["features_cal_vel","features_org_vel",'label', 'peopleTrialKey', 'start_seconds', 'end_seconds']) ])   
 
-
-    crash_feature_label_300ms_500ms_test.to_pickle('data/'+ 'crash_feature_label_'+ for_names+ 'ms' + '_500ms_test')
+    ## save it as pickle for training
+    crash_feature_label_300ms_500ms_test.to_pickle('data/data_1000ms/'+ 'crash_feature_label_'+ for_names+ 'ms' + '_1000ms_test')
 
 
 
     
-    ####### crash event data info with features within 0.5 second previous data ######
-    # time_scale_train = 0.5
-    # time_ahead = 0.3
-    # time_step = 0.3
-    # sampling_rate = 50
+    ####### crash event data info  ######
+
     peopleTrialHasCrash_ex = crash_excludeShortT.peopleTrialKey.unique()
 
     noncrash_feature_label_300ms_500ms_test = pd.DataFrame()
@@ -150,16 +174,13 @@ def generate_features(time_scale_train = 0.5,
             temp_serie = df_trial.loc[(df_trial.seconds>=left) & (df_trial.seconds<=right) \
                               ,'seconds'] 
             
-            
+            ## run sliding window on noncrash event
             list_all = sliding_window(temp_serie, time_scale_train, time_step) 
 
-            print('====================', len(list_all))
-            print('----------------------', len(list_all[0]))
-            print('xxxxxxxxxxxxxxxxxx', len(list_all[1]))
+
 
             if len(list_all) <1:
                 break
-
             for x in range(len(list_all)):
                 temp_df = df_trial[(df_trial.seconds >= list_all[x].iloc[0])\
                             & (df_trial.seconds <= list_all[x].iloc[-1])]
@@ -191,12 +212,12 @@ def generate_features(time_scale_train = 0.5,
                                 [noncrash_feature_label_300ms_500ms_test, pd.DataFrame([[arr11, arr22, arr33, arr44, arr55, arr66]],\
                                 columns=["features_cal_vel","features_org_vel",'label', 'peopleTrialKey', 'start_seconds', 'end_seconds']) ]) 
 
-
-    noncrash_feature_label_300ms_500ms_test.to_pickle('data/'+ 'noncrash_feature_label_'+ for_names+ 'ms' + '_500ms_test')
+    ## save it as pickle for training
+    noncrash_feature_label_300ms_500ms_test.to_pickle('data/data_1000ms/'+ 'noncrash_feature_label_'+ for_names+ 'ms' + '_1000ms_test')
 
 if __name__ == "__main__":
-    generate_features(time_scale_train = 0.5,
+    generate_features(time_scale_train = 1.0,
     time_ahead = 0.5,
     sampling_rate = 50,
-    time_gap=3,
-    time_step = 0.3)
+    time_gap=5,
+    time_step = 0.5)
--- Codes/model_validation_false_positive.py
+++ Codes/model_validation_false_positive.py
@@ -24,39 +24,46 @@ from sklearn.metrics import confusion_matrix
 
 
 
-#### read all the positive samples
-crash_feature_label_300ms_500ms = pd.read_pickle('data/data_1000ms/crash_feature_label_500ms_1000ms_test')
+# os.chdir('data')
+##### get all the files that contain ".csv" #######
+# all_path = glob.glob( '*/**.csv' )
 
-### read all the negative samples
-noncrash_feature_label_300ms_500ms = pd.read_pickle('data/data_1000ms/noncrash_feature_label_500ms_1000ms_test')
+#### read all the positive cases
+crash_feature_label_300ms_500ms = pd.read_pickle('data/crash_feature_label_500ms_500ms_test')
+
+### read all the negative casese
+noncrash_feature_label_300ms_500ms = pd.read_pickle('data/noncrash_feature_label_500ms_500ms_test')
 
 #### merge both positive and negative together
-data_final = pd.concat([crash_feature_label_300ms_500ms, noncrash_feature_label_300ms_500ms])
-data_final = data_final[['features_cal_vel','features_org_vel','label']]
+records = pd.concat([crash_feature_label_300ms_500ms, noncrash_feature_label_300ms_500ms])
+#data_final = data_final[['features_cal_vel','features_org_vel','label']]
+training_all = records.iloc[:, [0,1,3,4,5]]
+testing_all = records.iloc[:,2]
 
+train_X, test_X, train_y, test_y = train_test_split(training_all, testing_all, test_size=0.2, random_state=42)
+
+train_X_final = np.array([np.vstack(i) for i in train_X.features_org_vel])
+test_X_final = np.array([np.vstack(i) for i in test_X.features_org_vel])
+
+X_train_org, X_test_org, y_train_org, y_test_org = train_X_final, test_X_final, train_y, test_y
 
 
 #### split the data with calculated velocity and original velocity seperately 
-
-###  calculated velocity
 # X_cal = data_final.features_cal_vel
 # X_cal = np.array([np.vstack(i) for i in X_cal])
 
+#X_org = data_final.features_org_vel
+#X_org = np.array([np.vstack(i) for i in X_org])
 
-### original velocity
-X_org = data_final.features_org_vel
-X_org = np.array([np.vstack(i) for i in X_org])
-
-y = np.array(data_final.label)
+#y = np.array(data_final.label)
+# y = to_categorical(y)
 
 # X_train_cal, X_test_cal, y_train_cal, y_test_cal = train_test_split(X_cal, y, test_size=0.2, random_state=42)
 
-X_train_org, X_test_org, y_train_org, y_test_org = train_test_split(X_org, y, test_size=0.2, random_state=42)
+#X_train_org, X_test_org, y_train_org, y_test_org = train_test_split(X_org, y, test_size=0.2, random_state=42)
 
 
 ##### make data into sequence for training
-
-###  calculated velocity
 # X_train_cal = sequence.pad_sequences(X_train_cal, maxlen=50, padding='post', dtype='float', truncating='post')
 # y_train_cal = np.array(y_train_cal).reshape(len(y_train_cal),1)
 
@@ -64,7 +71,7 @@ X_train_org, X_test_org, y_train_org, y_test_org = train_test_split(X_org, y, te
 # y_test_cal = np.array(y_test_cal).reshape(len(y_test_cal),1)
 
 
-### original velocity
+
 X_train_org = sequence.pad_sequences(X_train_org, maxlen=50, padding='post', dtype='float', truncating='post')
 y_train_org = np.array(y_train_org).reshape(len(y_train_org),1)
 
@@ -77,6 +84,13 @@ enc = OneHotEncoder(handle_unknown='ignore', sparse=False)
 enc = enc.fit(y_train_org)
 y_train_org = enc.transform(y_train_org)
 y_test_org = enc.transform(y_test_org)
+
+
+# print('..........', y_train_cal.shape)
+
+# enc_org = enc.fit(y_train_org)
+# y_train_org = enc.transform(y_train_org)
+# y_test_org = enc.transform(y_test_org)
 
 
 ### train model
@@ -105,13 +119,8 @@ class_weights = [{
 { 
     0:1,
     1:10
-},
-{ 
-    0:1,
-    1:50
 }]
 
-### try different weights 
 for i in range(len(class_weights)):
     print('------------------------', i)
     history = model.fit(
@@ -133,18 +142,21 @@ for i in range(len(class_weights)):
 
     testing_org = y_test_org[:,0]
 
-    ### confusion matrix
-    cf_array_org = confusion_matrix(testing_org, predictions_org)
+    ### get the validation results to do feature engineering
+    ### here validation results means False Positive results, we want to see if there are any patterns for those samples
+    validation_final = test_X
+    validation_final['prediction'] = predictions_org
+    validation_final['ground_truth'] = testing_org
+    validation_final = validation_final[(validation_final.prediction != validation_final.ground_truth) & (validation_final.ground_truth==1.0)].iloc[:,2:]
+    validation_final.to_csv('results/results_validation/'+str(i)+'_validation_500ms_500ms.csv')  
 
-    ### save the results with accuracy, recall, precision
-    df_resutls = pd.DataFrame(cf_array_org)
-    ### accuracy
-    df_resutls['accuracy'] = (df_resutls.iloc[0,0] + df_resutls.iloc[1,1])/np.sum(cf_array_org)
-    ### recall
-    df_resutls['recall'] = df_resutls.iloc[0,0]/(df_resutls.iloc[0,0] + df_resutls.iloc[0,1])
-    ### precision
-    df_resutls['precision'] = df_resutls.iloc[0,0]/(df_resutls.iloc[0,0] + df_resutls.iloc[1,0])
-    df_resutls.to_csv('results/results_1000/'+ str(i)+'_500ms_1000ms_original_vel'+'.csv')
+ ### confusion matrix
+    #cf_array_org = confusion_matrix(testing_org, predictions_org)
+    #df_resutls = pd.DataFrame(cf_array_org)
+    #df_resutls['accuracy'] = (df_resutls.iloc[0,0] + df_resutls.iloc[1,1])/np.sum(cf_array_org)
+    #df_resutls['recall'] = df_resutls.iloc[0,0]/(df_resutls.iloc[0,0] + df_resutls.iloc[0,1])
+    #df_resutls['precision'] = df_resutls.iloc[0,0]/(df_resutls.iloc[0,0] + df_resutls.iloc[1,0])
+    #df_resutls.to_csv('results/results_1000/'+ str(i)+'_500ms_1000ms_original_vel'+'.csv')
 
     # print(predictions_cal.shape, testing_cal.shape)
 
